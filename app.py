25d08caedb8c9c0d9e1a5bfa71145a6bd31de4dd
Nithiiyan Skhanthan <nithdotdev@gmail.com>
2020-03-26 16:59:42 -0400

    app.py working

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
 from flask import Flask
+import psycopg2
+
 app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello, Flask!"
+
+
+
+try:
+    connection = psycopg2.connect(user = "postgres",
+                                  password = ";",
+                                  host = "127.0.0.1",
+                                  port = "5432",
+                                  database = "airbnb")
+
+    cursor = connection.cursor()
+    # Print PostgreSQL Connection properties
+    print ( connection.get_dsn_parameters(),"\n")
+
+    # Print PostgreSQL version
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You are connected to - ", record,"\n")
+
+except (Exception, psycopg2.Error) as error :
+    print ("Error while connecting to PostgreSQL", error)
+finally:
+    #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
