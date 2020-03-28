c14e322bc4ba3e21eb5794d8ca0480316bf7adfd
Yanbing Ren <yren020@uottawa.ca>
2020-03-27 22:51:07 -0400

    user:admin pw:ad123

--- app.py
+++ app.py
@@ -1,34 +1,43 @@
-# from flask import Flask
 import psycopg2
 
-# app = Flask(__name__)
-# @app.route("/")
-# def home():
-#     return "Hello, Flask!"
-
-
-
-try:
-    connection = psycopg2.connect(user = "yren020",
-                                  password = "",
+def open_connection():
+ try:
+   connection = psycopg2.connect(user = "nskha101",
+                                  password = "Nithrocksu25",
                                   host = "web0.eecs.uottawa.ca",
                                   port = "15432",
-                                  database = "yren020")
+                                  database = "nskha101")
 
-    cursor = connection.cursor()
-    # Print PostgreSQL Connection properties
-    print ( connection.get_dsn_parameters(),"\n")
+   cursor = connection.cursor()
+   # Print PostgreSQL Connection properties
+   print ( connection.get_dsn_parameters(),"\n")
 
     # Print PostgreSQL version
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record,"\n")
-
-except (Exception, psycopg2.Error) as error :
-    print ("Error while connecting to PostgreSQL", error)
-finally:
+   cursor.execute("SELECT version();")
+   record = cursor.fetchone()
+   print("You are connected to - ", record,"\n")
+ 
+ except (Exception, psycopg2.Error) as error:
+        print ("Error while connecting to PostgreSQL", error)
+#finally:
     #closing database connection.
-        if(connection):
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
+   
+def query(query):
+   record = cursor.fetchone()
+   print("You are connected to - ", record,"\n")
+    
+   cursor.execute("SELECT * from bnb")
+   print("Selecting rows from bnb table using cursor.fetchall")
+   bnb = cursor.fetchall() 
+   
+   print("Print each row and it's columns values")
+   for row in bnb:
+     for item in row:
+         print(row[item])
+   close()
+
+def close():
+   if(connection):
+      cursor.close()
+      connection.close()
+      print("PostgreSQL connection is closed")
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+
 from flask import Flask, redirect, url_for, request,render_template
 import psycopg2
+from app import *
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,53 +10,24 @@ def home():
    return render_template("login.html")
 
 
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
+@app.route('/admin')
+def admin():
+   return render_template('admin.html')
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
       user = request.form['un']
       password = request.form['pw']
-      return redirect(url_for('success',name = user))
+      if user.__eq__('admin') and password.__eq__('ad123'):
+         return redirect(url_for('admin'))
    else:
       user = request.args.get('un')
       password = request.args.get('pw')
       
       return redirect(url_for('success',name = user))
 
-try:
-   connection = psycopg2.connect(user = "yren020",
-                                  password = "",
-                                  host = "web0.eecs.uottawa.ca",
-                                  port = "15432",
-                                  database = "yren020")
-
-   cursor = connection.cursor()
-   # Print PostgreSQL Connection properties
-   print ( connection.get_dsn_parameters(),"\n")
-
-    # Print PostgreSQL version
-   cursor.execute("SELECT version();")
-   record = cursor.fetchone()
-   print("You are connected to - ", record,"\n")
- 
-
-except (Exception, psycopg2.Error) as error :
-    print ("Error while connecting to PostgreSQL", error)
-finally:
-    #closing database connection.
-   if(connection):
-      cursor.close()
-      connection.close()
-      print("PostgreSQL connection is closed")
-
-
-# @app.route('/admin')
-# def admin():
-#    return
+open_connection()
 
 if __name__ == "__main__":
    app.run(debug=True)
-
