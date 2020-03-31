8eed76f67c57ff8f82550967c6e109a0871c0724
Yanbing Ren <yren020@uottawa.ca>
2020-03-31 18:12:24 -0400

    radio button not working

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import Flask, redirect, url_for, request,render_template
 import psycopg2
 from app import *
 
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,30 +11,48 @@ def home():
    return render_template("login.html")
 
 
-@app.route('/admin')
+@app.route('/admin',methods=["POST"])
 def admin():
    return render_template('admin.html')
 
-@app.route('/guest')
-def guest():
-   return render_template('guest.html')
+
+
+@app.route('/host/<name>',methods=['POST'])
+def host_name(name):
+   return render_template('host.html',name=name)
+@app.route('/guest/<name>',methods=["POST"])
+def guest_name(name):
+   return render_template('guest.html', name=name)
+
+
+
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
 
       user = request.form['un']
       password = request.form['pw']
-      
+      option = request.form['options']
+
       if user.__eq__('admin') and password.__eq__('ad123'):
-      
          return redirect(url_for('admin'))
+      elif user.__eq__("guest") and password.__eq__('guest123') :
+         return redirect(url_for("guest_name", name=user))
+      elif user.__eq__("host") and password.__eq__('host123'):
+         return redirect(url_for("host_name"),name=user)
    else:
       user = request.args.get('un')
       password = request.args.get('pw')
-      
-      return redirect(url_for('success',name = user))
+      option = request.form['options']
 
-open_connection()
+      if user.__eq__('admin') and password.__eq__('ad123'):
+         return redirect(url_for('admin'))
+      elif user.__eq__("guest") and password.__eq__('guest123') and option.__eq__("guest"):
+         return redirect(url_for("guest_name", name=user))
+      elif user.__eq__("host") and password.__eq__('host123'):
+         return redirect(url_for("host_name"),name=user)
+
+# app.open_connection()
 
 if __name__ == "__main__":
    app.run(debug=True)
