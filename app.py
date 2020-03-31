a0e9eee6a2f2841052e43b61360e4c3877256f73
Nithiiyan Skhanthan <nithdotdev@gmail.com>
2020-03-31 17:16:58 -0400

    app.py update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import psycopg2
-
+import random
 def open_connection():
  try:
    connection = psycopg2.connect(user = "nskha101",
@@ -34,10 +34,29 @@ def query(query):
    for row in bnb:
      for item in row:
          print(row[item])
-   close()
+    cursor.close()
+   
 
+def uploadlisting(name, email, location, duration, maxsize, language, description, rules, cost, availability, contact):
+    try:
+        cursor = conn.cursor()
+        bnbid=random.randint(1,10000)+name
+        cursor.execute("INSERT INTO bnb (bnbid, email, name, location, duration, maxsize, langugage, description, rules, cost, availability, contact) VALUES(%s, %s, %s)", (bnbid, email, name, location, duration, maxsize, language, description, rules, cost, availability, contact))
+        conn.commit()
+        cursor.close()
+    except (Exception, psycopg2.Error) as error :
+        print("exception, yo")
+
+def availability(name):
+    cursor = conn.cursor()
+    query = "SELECT availablity, cost FROM bnb WHERE name = " + name
+    cursor.execute(query)
+    availability = cursor.fetchall()
+    for row in availability:
+        print("availability = ", row[0])
+        print("cost = ", row[1], "/n")
+    
 def close():
    if(connection):
-      cursor.close()
       connection.close()
       print("PostgreSQL connection is closed")
