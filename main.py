89ade2526a9aae7af03264a1f8b3bb59200cf221
Yanbing Ren <yren020@uottawa.ca>
2020-04-01 17:14:14 -0400

    host functionality added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, redirect, url_for, request,render_template
 import psycopg2
-from app import *
+from app import *  
 
 
 app = Flask(__name__)
@@ -10,49 +10,59 @@ app = Flask(__name__)
 def home():
    return render_template("login.html")
 
-
-@app.route('/admin',methods=["POST"])
+@app.route('/admin')
 def admin():
    return render_template('admin.html')
 
-
-
-@app.route('/host/<name>',methods=['POST'])
+@app.route('/host/<name>')
 def host_name(name):
    return render_template('host.html',name=name)
-@app.route('/guest/<name>',methods=["POST"])
+
+@app.route('/update_info')
+def update_info():
+   return render_template('host_info.html')
+
+
+@app.route('/guest/<name>')
 def guest_name(name):
    return render_template('guest.html', name=name)
 
 
+@app.route('/employee/<name>')
+def employee_name(name):
+   return render_template('employee.html',name=name)
 
-@app.route('/login',methods = ['POST', 'GET'])
+@app.route('/result')
+def result():
+   if request.method.__eq__('POST'):
+      result = request.form
+      return render_template('search_result.html',result=result)
+
+@app.route('/login',methods = ['POST','GET'])
 def login():
-   if request.method == 'POST':
 
-      user = request.form['un']
-      password = request.form['pw']
-      option = request.form['options']
+   error=''
+   try:
 
-      if user.__eq__('admin') and password.__eq__('ad123'):
-         return redirect(url_for('admin'))
-      elif user.__eq__("guest") and password.__eq__('guest123') :
-         return redirect(url_for("guest_name", name=user))
-      elif user.__eq__("host") and password.__eq__('host123'):
-         return redirect(url_for("host_name"),name=user)
-   else:
-      user = request.args.get('un')
-      password = request.args.get('pw')
-      option = request.form['options']
+      if request.method == 'POST':
+         user = request.form['un']
+         password = request.form['pw']
+         option = request.form['options']
+         if user.__eq__('admin') and password.__eq__('ad123'):
+            return redirect(url_for('admin'))
+         elif user.__eq__("guest") and password.__eq__('guest123') and option == 'guest' :
+            return redirect(url_for("guest_name", name=user))
+         elif user.__eq__("host") and password.__eq__('host123') and option == 'host':
+            return redirect(url_for("host_name",name=user))
+         else:
+            error='Invalid username or password, please try again!'
+      return render_template('login.html',error=error)
 
-      if user.__eq__('admin') and password.__eq__('ad123'):
-         return redirect(url_for('admin'))
-      elif user.__eq__("guest") and password.__eq__('guest123') and option.__eq__("guest"):
-         return redirect(url_for("guest_name", name=user))
-      elif user.__eq__("host") and password.__eq__('host123'):
-         return redirect(url_for("host_name"),name=user)
+   except Exception as ex:
+      return render_template('login.html',error=error)
 
-# app.open_connection()
+
 
 if __name__ == "__main__":
    app.run(debug=True)
+   
